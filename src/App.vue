45b13e5aaab2475b3d04adb5db813faf2bb15914
adinar <adinaria223311@gmail.com>
2025-01-02 15:27:20 +0200

    main

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyHello from './components/common/MyHello.vue'
+import { MainButton } from 'vue-tg'
+</script>
+
+<template>
+  <header>
+    <MyHello />
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <MyHeader />
+    <TheWelcome />
+  </main>
+</template>
