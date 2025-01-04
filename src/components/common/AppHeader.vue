02d4eccbe64176faa72c4c5fa941095201568173
adinar <adinaria223311@gmail.com>
2025-01-04 03:16:07 +0200

    add main header

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,7 +1,58 @@
 <template>
   <header>
-    <h1>I am header</h1>
+    <div class="header-container">
+      <div class="header-info">
+        <div class="header-img">
+          <img :src="userDataTelegram?.photo_url" />
+          <!-- <img src="../icons/wallet.svg" /> -->
+        </div>
+        <span>
+          {{ userDataTelegram?.first_name + ' ' + userDataTelegram?.last_name }}
+        </span>
+      </div>
+    </div>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getUserData } from '../../services/Telegram'
+
+const userDataTelegram = getUserData()
+</script>
+
+<style>
+.header-container {
+  border-radius: 200px;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center; /* Вертикальное выравнивание контента */
+  color: #e2dbdb;
+  height: 50px;
+  margin-top: 20px;
+  opacity: 94%;
+  background: linear-gradient(rgb(23, 23, 27), rgb(12, 10, 10));
+}
+.header-info {
+  align-items: center; /* Вертикальное выравнивание контента */
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.header-img {
+  width: 40px; /* Ширина контейнера для изображения */
+  height: 40px; /* Высота контейнера для изображения */
+  overflow: hidden; /* Скрыть всё, что выходит за пределы контейнера */
+  padding-left: 15px;
+  padding-right: 10px;
+  padding-bottom: 7px;
+  padding-top: 7px;
+}
+
+.header-img img {
+  border-radius: 50%; /* Сделать изображение круглым */
+  width: 100%; /* Изображение будет растягиваться по ширине контейнера */
+  height: 100%; /* Изображение будет растягиваться по высоте контейнера */
+  object-fit: contain; /* Изображение будет заполненно, чтобы заполнить контейнер */
+  image-rendering: optimizeQuality; /* для улучшения качества */
+}
+</style>
--- src/services/Telegram.ts
+++ src/services/Telegram.ts
@@ -1,5 +1,32 @@
-// import WebApp from '@twa-dev/sdk'
+import WebApp from '@twa-dev/sdk'
+import type { WebAppUser } from '@twa-dev/types'
 
-// function getInitData(): string {
-//   return WebApp.initData
+// export interface UserDataTelegram {
+//   full_name: string | undefined
+//   photo_url: string | undefined
 // }
+
+// export const userData = (): UserDataTelegram | undefined => {
+//   console.log(WebApp.initDataUnsafe.user?.first_name)
+//   const userTelegram = WebApp.initDataUnsafe.user
+
+//   if (typeof userTelegram === 'undefined') {
+//     return undefined
+//   }
+
+//   const user: UserDataTelegram = {
+//     full_name: userTelegram.first_name + ' ' + userTelegram.last_name,
+//     photo_url: userTelegram.photo_url,
+//   }
+
+//   return user
+// }
+
+export const getUserData = ():
+  | (WebAppUser & {
+      added_to_attachment_menu?: boolean
+      allows_write_to_pm?: boolean
+    })
+  | undefined => {
+  return WebApp.initDataUnsafe?.user
+}
