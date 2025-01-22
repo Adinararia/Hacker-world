6ec1d71c26e926290847c3406a55ef2b6f4b7d7b
adinar <adinaria223311@gmail.com>
2025-01-22 02:27:12 +0200

    fix main.css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import AppHeader from './components/common/AppHeader.vue'
 import './assets/base.css'
+import AppHeader from './components/common/AppHeader.vue'
 import AppFooter from './components/common/AppFooter.vue'
 </script>
 <template>
